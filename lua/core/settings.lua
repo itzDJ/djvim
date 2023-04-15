bdc81b1184fd18c4c5a3277b803a7efb9c21c856
itzDJ <djcerami@gmail.com>
2023-04-15 18:23:34 -0400

    renamed file

---
+++ lua/core/settings.lua
@@ -0,0 +1,10 @@
+-- (VIM) SETTINGS
+vim.opt.number = true -- enable numbers
+vim.opt.relativenumber = true -- enable relative numbers
+
+vim.opt.tabstop = 4 -- insert 4 spaces for a tab
+vim.opt.shiftwidth = 4 -- change the number of space characters inserted for indentation
+vim.opt.expandtab = true -- use spaces instead of tabs
+
+-- vim.opt.spelllang = en_us -- specific language not working
+-- vim.opt.spell = true -- enable spell check
