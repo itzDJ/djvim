087ec85968c3a2b9ff2900fa0342e981508e3dab
DJ <dj@Cheap-Guys-Mac-mini.local>
2023-07-04 11:18:26 -0400

    updated treesitter installation

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -70,7 +70,10 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
     }
 
     use {
