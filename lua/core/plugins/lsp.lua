1539f4477b744c2da7ba99b6c8dc62d183d20120
itzDJ <djcerami@gmail.com>
2023-07-25 11:34:22 -0400

    added ensure install lsps

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -6,13 +6,6 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
--- -- When you don't have mason.nvim installed
--- -- You'll need to list the servers installed in your system
--- lsp.setup_servers({'quick_lint_js', 'lua_ls', 'marksman', 'pyright'})
---
--- -- (Optional) Configure lua language server for neovim
--- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
 lsp.ensure_installed({
     "quick_lint_js", -- JavaScript
     "lua_ls", -- Lua
@@ -20,30 +13,15 @@ lsp.ensure_installed({
     "pyright", -- Python
 })
 
-lsp.setup()
+require'lspconfig'.lua_ls.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+        },
+    },
+}
 
--- require("mason").setup()
---
--- require("mason-lspconfig").setup({
---     ensure_installed = {
---         "quick_lint_js", -- JavaScript
---         "lua_ls", -- Lua
---         "marksman", -- Markdown
---         "pyright", -- Python
---     }
--- })
---
--- local lspconfig = require("lspconfig")
--- lspconfig.quick_lint_js.setup{}
--- lspconfig.lua_ls.setup{
---     settings = {
---         Lua = {
---             diagnostics = {
---                 globals = {"vim"}
---             }
---         }
---     }
--- }
--- lspconfig.marksman.setup{}
--- lspconfig.pyright.setup{}
---
+lsp.setup()
