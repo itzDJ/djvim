92cb5e2a8dfbe59cb4811f3116afb74a245f1560
DJ <djcerami@gmail.com>
2023-10-04 23:10:36 -0400

    bootstrapped formatters

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -1,4 +1,4 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
@@ -7,24 +7,24 @@ lsp_zero.on_attach(function(client, bufnr)
     lsp_zero.buffer_autoformat()
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
+require("mason").setup({})
+require("mason-lspconfig").setup({
     ensure_installed = {
         "tsserver", -- JavaScript
-        "lua_ls",   -- Lua
-        "pyright",  -- Python
+        "lua_ls", -- Lua
+        "pyright", -- Python
     },
     handlers = {
         lsp_zero.default_setup,
-    }
+    },
 })
 
-require('lspconfig').lua_ls.setup({
+require("lspconfig").lua_ls.setup({
     settings = {
         Lua = {
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
+                globals = { "vim" },
             },
         },
     },
@@ -34,3 +34,22 @@ lsp_zero.setup_servers({
     "tsserver",
     "pyright",
 })
+
+-- Custom function to install formatters (prettier)
+vim.api.nvim_create_user_command("InstallFormatters", function()
+    local formatters = {
+        "stylua",
+        "black",
+        "prettier",
+        "codespell",
+    }
+
+    for _, formatter in pairs(formatters) do
+        vim.cmd("MasonInstall " .. formatter)
+    end
+end, { nargs = "*", desc = "Install formatters through mason" })
+
+-- If prettier is not installed, install formatters
+if vim.fn.executable("prettier") ~= 1 then
+    vim.cmd("InstallFormatters")
+end
