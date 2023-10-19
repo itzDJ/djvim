0df27ae7db464a21ab0158452f4fbeb6b7a4b50e
DJ <djcerami@gmail.com>
2023-10-19 16:30:17 -0400

    forced default keymaps

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -3,8 +3,11 @@ local lsp_zero = require("lsp-zero")
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
-    lsp_zero.buffer_autoformat()
+    lsp_zero.default_keymaps({
+        buffer = bufnr,
+        preserve_mappings = false -- Needed to force the default mappings
+    })
+    lsp_zero.buffer_autoformat()  -- TODO Is this needed after adding conform?
 end)
 
 require("mason").setup({})
