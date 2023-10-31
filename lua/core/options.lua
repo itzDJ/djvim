6e054e4c7ff714bce206d4f84a002ae47518fd09
DJ <djcerami@gmail.com>
2023-10-30 20:10:40 -0400

    added comments

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,12 +2,14 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- Spell checking
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
 
