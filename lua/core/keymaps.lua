c3a8f5a0775d1db3c08fe2e23a61195f2c024012
DJ <djcerami@gmail.com>
2023-11-15 19:02:02 -0500

    fixed comment

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,15 +2,13 @@
 -- Note: <CR> is carriage return or enter
 vim.g.mapleader = " "                                                -- set leader key to space
 
-vim.keymap.set("n", "<c-s>", ":w<CR>", { noremap = true })           -- save file with <ctrl> + s
-
 vim.keymap.set("n", "<c-d>", "<c-d>zz", { noremap = true })          -- scroll down with <ctrl> + d and keep cursor in the middle
 vim.keymap.set("n", "<c-u>", "<c-u>zz", { noremap = true })          -- scroll up with <ctrl> + u and keep cursor in the middle
 
-vim.keymap.set("n", "<c-j>", ":m .+1<CR>==", { noremap = true })     -- move line down with <alt> + j
-vim.keymap.set("n", "<c-k>", ":m .-2<CR>==", { noremap = true })     -- move line up with <alt> + k
-vim.keymap.set("v", "<c-j>", ":m '>+1<CR>gv=gv", { noremap = true }) -- move lines down with <alt> + j
-vim.keymap.set("v", "<c-k>", ":m '<-2<CR>gv=gv", { noremap = true }) -- move lines up with <alt> + k
+vim.keymap.set("n", "<c-j>", ":m .+1<CR>==", { noremap = true })     -- move line down with <ctrl> + j
+vim.keymap.set("n", "<c-k>", ":m .-2<CR>==", { noremap = true })     -- move line up with <ctrl> + k
+vim.keymap.set("v", "<c-j>", ":m '>+1<CR>gv=gv", { noremap = true }) -- move lines down with <ctrl> + j
+vim.keymap.set("v", "<c-k>", ":m '<-2<CR>gv=gv", { noremap = true }) -- move lines up with <ctrl> + k
 
 vim.keymap.set("n", "<leader>t", ":Explore<CR>", { noremap = true }) -- open netrw with <leader> + t
 
