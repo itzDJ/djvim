338767a6e6f335687764cd4f7ffe76d1ac7f17c5
DJ <djcerami@gmail.com>
2024-09-16 09:57:41 -0400

    removed unused plugins

--- lua/plugins/nvim_colorizer.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "norcalli/nvim-colorizer.lua",
-    opts = {
-        -- only enable colorizer for css, html, and javascript
-        "css",
-        "html",
-        "javascript",
-    }
-}
