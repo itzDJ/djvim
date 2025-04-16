bfb4c8f237062bb4fd9ea2db4e20fcc472178800
DJ <djcerami@gmail.com>
2025-04-15 21:54:48 -0400

    added priority

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,16 @@
 return {
     "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
     config = function()
         require("github-theme").setup({
-            options = {
-                transparent = true,
-            },
+            options = { transparent = true },
         })
 
         -- If `echo $COLORTERM` returns 'truecolor\n' then enable the theme
         if io.popen("echo $COLORTERM"):read("*a") == "truecolor\n" then
             vim.cmd("colorscheme github_dark")
         end
-    end
+    end,
 }
